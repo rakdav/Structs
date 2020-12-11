bb2c4e0278e48a9df571c432f7cff7ebd85edfed
Andrey Dubinin <rakdav@inbox.ru>
2020-12-11 16:58:14 +0200

    Добавьте файлы проекта.

---
+++ Structs/MainWindow.xaml.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Structs
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    ///
+    public struct Person
+    {
+        public string fio;
+        public DateTime? dat;
+        public char pol;
+        public double rost;
+        public double ves;
+        public bool ekat;
+    }
+    public partial class MainWindow : Window
+    {
+        Person[] persons;
+        int i = 0;
+        int count;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(i==0)
+            {
+                try
+                {
+                    count = int.Parse(Count.Text);
+                    persons = new Person[count];
+                    AddPerson();
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+            {
+                AddPerson();
+            }
+            if(i==count)
+            {
+                Add.IsEnabled = false;
+            }
+        }
+        public void AddPerson()
+        {
+            if (i < count)
+            {
+                WindowPerson window = new WindowPerson();
+                if (window.ShowDialog() == true)
+                {
+                    Person p = new Person();
+                    p.fio = window.getFio();
+                    p.dat = window.getBirth();
+                    p.pol = window.getPol();
+                    p.rost = window.getRost();
+                    p.ves = window.getWeight();
+                    p.ekat = window.getEkat();
+                    persons[i] = p;
+                    Persons.Items.Add(p.fio);
+                }
+                i++;
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Add.IsEnabled = false;
+        }
+
+        private void Count_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Add.IsEnabled = true;
+        }
+
+        private void Persons_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            string f = Persons.SelectedItems[0].ToString();
+            Person p;
+            for(int i=0;i<count;i++)
+            {
+                if(persons[i].fio.Equals(f))
+                {
+                    p = persons[i];
+                    WindowPerson w = new WindowPerson(p);
+                    w.Show();
+                    break;
+                }
+            }
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            for(int i=0;i<count-1;i++)
+            {
+                for(int j=i+1;j<count;j++)
+                {
+                    if(persons[i].dat>persons[j].dat)
+                    {
+                        Person p = new Person();
+                        p = persons[i];
+                        persons[i] = persons[j];
+                        persons[j] = p;
+                    }
+                }
+            }
+            Persons.Items.Clear();
+            for (int i = 0; i < count; i++) Persons.Items.Add(persons[i].fio);
+
+        }
+    }
+}
---
+++ Structs/WindowPerson.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Structs
+{
+    /// <summary>
+    /// Логика взаимодействия для WindowPerson.xaml
+    /// </summary>
+    public partial class WindowPerson : Window
+    {
+        public WindowPerson(Person p)
+        {
+            InitializeComponent();
+            FIO.Text = p.fio;
+            DateBirth.SelectedDate= p.dat;
+            Rost.Text = p.rost.ToString();
+            Weight.Text = p.ves.ToString();
+            if (p.pol == 'М') man.IsChecked = true;
+            else woman.IsChecked = true;
+            if (p.ekat == true) Ekat.IsChecked = true;
+        }
+        public WindowPerson()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+
+        public string getFio()
+        {
+            return FIO.Text;
+        }
+
+        public DateTime? getBirth()
+        {
+            return DateBirth.SelectedDate;
+        }
+
+        public double getRost()
+        {
+            return double.Parse(Rost.Text);
+        }
+
+        public double getWeight()
+        {
+            return double.Parse(Weight.Text);
+        }
+        public char getPol()
+        {
+            if (man.IsChecked == true) return 'М';
+            return 'Ж';
+        }
+
+        public bool getEkat()
+        {
+            if(Ekat.IsChecked==true) return true;
+            return false;
+        }
+    }
+}
